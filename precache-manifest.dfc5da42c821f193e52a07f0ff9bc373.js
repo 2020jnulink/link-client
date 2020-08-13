fee72352847df96d654dd3d8c78255b0e474dd65
Jeongmin <wjd326als@naver.com>
2020-08-13 14:12:46 +0900

    Updates

---
+++ precache-manifest.dfc5da42c821f193e52a07f0ff9bc373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56ca7396d4b92a1bb6ddc86da485d2d",
+    "url": "/link-client/index.html"
+  },
+  {
+    "revision": "cd8da63d64d511c32813",
+    "url": "/link-client/static/css/main.2bae1d8e.chunk.css"
+  },
+  {
+    "revision": "f1f64867f58af61878a3",
+    "url": "/link-client/static/js/2.7f1333ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/link-client/static/js/2.7f1333ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8da63d64d511c32813",
+    "url": "/link-client/static/js/main.0e34b572.chunk.js"
+  },
+  {
+    "revision": "48767a98df5d9cc10bce",
+    "url": "/link-client/static/js/runtime-main.7c7ceaaa.js"
+  },
+  {
+    "revision": "1e18d68153c2c6b33f6137f52143e3d9",
+    "url": "/link-client/static/media/home.1e18d681.jpg"
+  }
+]);
