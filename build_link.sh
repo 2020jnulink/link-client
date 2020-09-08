90451eac088c2a92806537219c11d14064c2db99
simeunseok <50517813+simeunseok@users.noreply.github.com>
2020-09-08 09:55:42 +0900

    Add files via upload

--- build_link.sh
+++ build_link.sh
@@ -2,6 +2,6 @@
 cp ./binary_ubuntu/* .
 export FABRIC_CFG_PATH=$PWD
 sh ./link_fabric.sh
-# sh ./docker-images.sh
-# sleep 5
-# docker-compose up -d
+sh ./docker-images.sh
+sleep 5
+docker-compose up -d
