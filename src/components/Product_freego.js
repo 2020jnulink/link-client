c0c436ecf38ad6a77dbeb2a09b81e86ea0259e89
seonju317 <68142159+seonju317@users.noreply.github.com>
2020-09-15 00:04:30 +0900

    0915

--- src/components/Product_freego.js
+++ src/components/Product_freego.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import product_img from "./img/freego.png";
 import Navigation_var from "./Navigation_var"; // eslint-disable-line no-unused-vars
-import "./Product.css";
+import "./Product_freego.css";
 
 class Product extends React.Component {
   render() {
